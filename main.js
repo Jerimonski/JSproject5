716c688817a3dab954f3cf81261542a85af2ff30
jeremy <jerimonski@gmail.com>
2024-09-05 17:58:25 -0400

    finished

--- main.js
+++ main.js
@@ -1,8 +1,13 @@
 const SearchInput = document.getElementById("name_or_id_input");
 const btn = document.getElementById("btn");
 const pokemon_container = document.getElementById("pokemon_container");
-const poke_stats = document.getElementById("poke_stats");
 
+const hp_ = document.getElementById("hp");
+const attack = document.getElementById("attack");
+const defense = document.getElementById("defense");
+const specialAttack = document.getElementById("special-attack");
+const specialDefense = document.getElementById("special-defense");
+const speed = document.getElementById("speed");
 
 btn.addEventListener("click", () => {
     const API_URL = `https://pokeapi-proxy.freecodecamp.rocks/api/pokemon/${SearchInput.value}`;
@@ -17,29 +22,22 @@ const ShowData = (data) => {
     let PokemonName = document.createRange().createContextualFragment(`
         <div class="img">
             <figure>
-            <img src="${data.sprites.front_default}">
+            <img class="poke_img" src="${data.sprites.front_default}">
             <figcaption>${data.name} #${data.id}</figcaption>
             </figure>
         </div>
-        ${data.types.map(obj => `<span class="${obj.type.name}">${obj.type.name}</span>`).join(' ')}
+        ${data.types.map(obj => `<span class="types ${obj.type.name}">${obj.type.name}</span>`).join(' ')}
     `);
-    // append stats to table_stats rows
-    let PokemonStats = document.createRange().createContextualFragment(`
-        ${data.stats.map(obj => `<span class="stats ${obj.base_stat}">${obj.base_stat}</span>`).join(' ')}
-    `);
-    //use this to create a span for each type and add classes 
-// Set types
-// types.innerHTML = data.types
-// .map(obj => `<span class="type ${obj.type.name}">${obj.type.name}</span>`)
-// .join('');
-    poke_stats.appendChild(PokemonStats);
+    
+    hp.textContent = data.stats[0].base_stat;
+    attack.textContent = data.stats[1].base_stat;
+    defense.textContent = data.stats[2].base_stat;
+    specialAttack.textContent = data.stats[3].base_stat;
+    specialDefense.textContent = data.stats[4].base_stat;
+    speed.textContent = data.stats[5].base_stat;
+
     pokemon_container.appendChild(PokemonName);
 }
-
-    /*
-        TODO: Change for loop, instead use just data.results.name or id,
-        TODO: or look for a another way to solve bugs 
-    */
 
 
 
